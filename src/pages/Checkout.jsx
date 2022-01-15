cf66815877c653239baf8a085d20247895581330
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2022-01-15 19:21:57 +0300

    add payment method

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
-import { Typography, Box } from '@mui/material';
+import { Typography, Box, Container, Grid } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 
 import { Logo } from '../components/Logo';
+import { PaymentMethod } from '../components/PaymentMethod';
+
+const useStyles = makeStyles({
+    root: {
+        padding: '1rem',
+    },
+    paymentGrid: {
+        backgroundColor: '#fafafa',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+});
 
 const Checkout = () => {
+    const classes = useStyles();
+
     return (
-        <Box>
-            <Logo />
+        <Box className={classes.root}>
+            <Grid container sx={{ backgroundColor: 'blue' }}>
+                <Grid item xs={6} className={classes.paymentGrid}>
+                    <Logo />
+                    <Container maxWidth='md'>
+                        <PaymentMethod />
+                    </Container>
+                </Grid>
+                <Grid item xs={6}>
+                    <Container maxWidth='md' sx={{ backgroundColor: 'pink' }}>
+                        <Logo />
+                    </Container>
+                </Grid>
+            </Grid>
         </Box>
     )
 }
