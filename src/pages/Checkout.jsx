675c931e3071e186e210f3962ed37a8b7b2fce42
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2022-01-15 18:47:08 +0300

    add fonts

---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+const Checkout = () => {
+    return (
+        <div>
+            <Typography variant="h1">Checkout</Typography>
+        </div>
+    )
+}
+
+export default Checkout;
