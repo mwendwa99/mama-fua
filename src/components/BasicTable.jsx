ba1fcc823b5b55715c72b737aeb7d0f0017cdb84
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2022-01-16 10:57:28 +0300

    Update BasicTable

--- src/components/BasicTable.jsx
+++ src/components/BasicTable.jsx
@@ -1,49 +1,81 @@
 import * as React from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import Divider from '@mui/material/Divider';
 import Box from '@mui/material/Box';
 import { Typography } from '@mui/material';
 
-function createData(name, calories, total) {
-    return { name, calories, total };
+function createData(name, price) {
+    return { name, price };
 }
 
 const rows = [
-    createData('Frozen yoghurt', 159),
-    createData('Ice cream sandwich', 237),
-    createData('Total', 1400),
+    createData('Bucket Wash', 400),
+    createData('Kitchen Wash', 600),
 ];
 
 export default function BasicTable() {
     return (
         <Box sx={{ pt: '2rem' }}>
             <TableContainer>
-                <Typography variant='h6' >Cart Items</Typography>
-                <Table aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Dessert (100g serving)</TableCell>
-                            <TableCell align="right">Calories</TableCell>
-                        </TableRow>
-                    </TableHead>
+                <Typography variant='h6' fontWeight={700} >Cart Items</Typography>
+                <Divider />
+                <Table aria-label="simple table"
+                    sx={{
+                        [`& .${tableCellClasses.root}`]: {
+                            borderBottom: "none"
+                        }
+                    }}
+                >
                     <TableBody>
                         {rows.map((row) => (
                             <TableRow
                                 key={row.name}
                                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                             >
-                                <TableCell component="th" scope="row">
-                                    {row.name}
+                                {/* <TableCell component="th" scope="row"> */}
+                                <TableCell align='left'>
+                                    <Typography variant='body1'>{row.name}</Typography>
                                 </TableCell>
-                                <TableCell align="right">{row.calories}</TableCell>
-                                <TableCell align="right">{row.fat}</TableCell>
+                                <TableCell align="right">
+                                    <Typography variant='body1'>Kes. {row.price}</Typography>
+                                </TableCell>
                             </TableRow>
                         ))}
+                    </TableBody>
+                    <TableBody>
+                        <TableRow>
+                            {/* add subtotal */}
+                            <TableCell>
+                                <Typography variant='body1'>Subtotal</Typography>
+                            </TableCell>
+                            <TableCell align="right">
+                                <Typography variant='body1' fontWeight={700}>{rows.reduce((acc, cur) => acc + cur.price, 0)}</Typography>
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            {/* add tax */}
+                            <TableCell>
+                                <Typography variant='body1'>Tax</Typography>
+                            </TableCell>
+                            <TableCell align="right">
+                                <Typography variant='body1' fontWeight={700}>{rows.reduce((acc, curr) => acc + curr.price, 0) * 16 / 100}</Typography>
+                            </TableCell>
+
+                        </TableRow>
+                        <TableRow>
+                            {/* add total */}
+                            <TableCell>
+                                <Typography variant='h6' fontWeight={700} >Total</Typography>
+                            </TableCell>
+                            <TableCell align="right">
+                                {/* round upward */}
+                                <Typography variant='body1' fontWeight={700}>{Math.ceil(rows.reduce((acc, curr) => acc + curr.price, 0) * 1.16)}</Typography>
+                            </TableCell>
+                        </TableRow>
                     </TableBody>
                 </Table>
             </TableContainer>
