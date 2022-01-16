d8c6c62b90b90e14aebf4699da20f57c40ef1998
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2022-01-16 11:00:14 +0300

    add Divider to BasicTable

--- src/components/BasicTable.jsx
+++ src/components/BasicTable.jsx
@@ -66,6 +66,7 @@ export default function BasicTable() {
                             </TableCell>
 
                         </TableRow>
+                        <Divider />
                         <TableRow>
                             {/* add total */}
                             <TableCell>
